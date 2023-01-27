e2c49aba423bff969e3d5c940bc4b0abcf5b5d73
abenteuerzeit <amroz.muzyka@gmail.com>
2023-01-27 12:26:13 +0100

    implement ideas routes

--- server/routes/ideas.js
+++ server/routes/ideas.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const ideasRouter = express.Router();
-const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require('../db.js');
+const { getAllFromDatabase, addToDatabase, getFromDatabaseById, updateInstanceInDatabase, deleteFromDatabasebyId } = require('../db.js');
 
+// Route /api/ideas
 
+// GET /api/ideas to get an array of all ideas.
+ideasRouter.get('/', (req, res, next) => {
+    const ideas = getAllFromDatabase('ideas');
+    res.status(200).send(ideas);
+});
 
+// POST /api/ideas to create a new idea and save it to the database.
+ideasRouter.post('/', (req, res, next) => {
+    const newIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
+});
+
+ideasRouter.param('ideaId', (req, res, next, ideaId) => {
+    const idea = getFromDatabaseById('ideas', ideaId);
+    if (idea) {
+        req.idea = idea;
+        next();
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+// GET /api/ideas/:ideaId to get a single idea by id.
+ideasRouter.get('/:ideaId', (req, res, next) => {
+    res.status(200).send(req.idea);
+});
+
+// PUT /api/ideas/:ideaId to update a single idea by id.
+ideasRouter.put('/:ideaId', (req, res, next) => {
+    const updatedIdea = Object.assign(req.idea, req.body);
+    const updated = updateInstanceInDatabase('ideas', updatedIdea);
+    res.status(200).send(updated);
+});
+
+// DELETE /api/ideas/:ideaId to delete a single idea by id.
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+    const deletedIdea = deleteFromDatabasebyId('ideas',req.idea.id);
+    res.status(204).send(deletedIdea);
+});
 
 module.exports = ideasRouter;
