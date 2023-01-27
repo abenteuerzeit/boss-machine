74a974ca3395341abaf10656b67973d1682a0bfa
abenteuerzeit <amroz.muzyka@gmail.com>
2023-01-27 11:47:38 +0100

    implement minions api

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
 module.exports = app;
 
@@ -9,17 +12,29 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
+app.use(bodyParser.json());
 
+// Logging
+app.use(morgan('dev'));
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
 
+apiRouter.get('/', (req, res, next) => {
+  res.send({data: 'hello world'});
+});
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`)
+    console.log('Press Ctrl+C to quit.');
+  });
 }
+
---
+++ server/minions/minions.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const minionsRouter = express.Router();
+const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require('../db.js');
+
+// Route /api/minions
+
+// GET /api/minions to get an array of all minions.
+minionsRouter.get('/', (req, res, next) => {
+    const minions = getAllFromDatabase('minions');
+    res.send(minions);
+});
+
+minionsRouter.param('minionId', (req, res, next, minionId) => {
+    const minion = getFromDatabaseById('minions', minionId);
+    if (minion) {
+        req.minion = minion;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+// GET /api/minions/:minionId to get a single minion by id.
+minionsRouter.get('/:minionId', (req, res, next) => {
+    res.status(200).send(req.minion);
+});
+
+// POST /api/minions to create a new minion and save it to the database.
+minionsRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+});
+
+// PUT /api/minions/:minionId to update a single minion by id.
+minionsRouter.put('/:minionId', (req, res, next) => {
+    const updatedMinion = Object.assign(req.minion, req.body);
+    const updated = updateInstanceInDatabase('minions', updatedMinion);
+    if (updated) {
+        res.status(200).send(updated);
+    } else {
+        res.status(500).send();
+    }
+});
+
+// DELETE /api/minions/:minionId to delete a single minion by id.
+minionsRouter.delete('/:minionId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('minions', req.minion.id);
+    if (deleted) {
+        res.status(204).send(deleted);
+    } else {
+        res.status(500).send();
+    }
+});
+
+module.exports = minionsRouter;
