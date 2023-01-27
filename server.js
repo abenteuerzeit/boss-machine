496e4297e0534011597d0afbb7a1d2f7f5750072
abenteuerzeit <amroz.muzyka@gmail.com>
2023-01-27 17:55:56 +0100

    netlify

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
+const apiRouter = require('./server/api');
 
-module.exports = app;
-
-/* Do not change the following line! It is required for testing and allowing
-*  the frontend application to interact as planned with the api server
-*/
+const app = express();
 const PORT = process.env.PORT || 4001;
 
-// Add middleware for handling CORS requests from index.html
 app.use(cors());
-
-// Add middware for parsing request bodies here:
 app.use(bodyParser.json());
-
-// Logging
 app.use(morgan('dev'));
-
-// Mount your existing apiRouter below at the '/api' path.
-const apiRouter = require('./server/api');
 app.use('/api', apiRouter);
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './index.html'));
+}); 
 
 app.use((err, req, res, next) => {
   console.error(err.message);
@@ -33,7 +27,8 @@ app.use((err, req, res, next) => {
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
   app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}. Example route: http://localhost:${PORT}/api/minions`)
+    console.log(`Server is listening on port ${PORT}`);
   });
 }
 
+module.exports = app;
